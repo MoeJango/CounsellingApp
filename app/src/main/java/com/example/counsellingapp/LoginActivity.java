17f646d9ea38134121bb3d6e349df3993abb37ce
Yusuf <moejoe1756@gmail.com>
2023-06-01 20:23:50 +0200

    Version 1

--- app/src/main/java/com/example/counsellingapp/LoginActivity.java
+++ app/src/main/java/com/example/counsellingapp/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.counsellingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -51,11 +52,15 @@ public class LoginActivity extends AppCompatActivity {
 
 
     public void onLoginClick(View view) {
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 
     @Override
     public void onBackPressed() {
+        super.onBackPressed();
         finish();
     }
 }
--- app/src/main/java/com/example/counsellingapp/RegisterActivity.java
+++ app/src/main/java/com/example/counsellingapp/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.counsellingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -69,5 +70,12 @@ public class RegisterActivity extends AppCompatActivity {
         //Intent intent = new Intent(RegistrationActivity.this, HomeActivity.class);
         //startActivity(intent);
         //finish();
+    }
+
+
+    public void onRegisterClick(View view) {
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
