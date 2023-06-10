09d9924e19dd010ea2e56c7c9810fb03963cf49c
Yusuf <moejoe1756@gmail.com>
2023-06-10 17:34:11 +0200

    Version 3

--- app/src/main/java/com/example/counsellingapp/ChatActivity.java
+++ app/src/main/java/com/example/counsellingapp/ChatActivity.java
@@ -1,5 +1,6 @@
 package com.example.counsellingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,7 +11,16 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -45,8 +55,8 @@ public class ChatActivity extends AppCompatActivity {
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!sendText.toString().isEmpty()) {
-
+                if ((!sendText.toString().isEmpty()) && (sendText.toString().length() < 255)) {
+                    insert(myID, receiverID, sendText.toString());
                 }
             }
         });
@@ -63,5 +73,37 @@ public class ChatActivity extends AppCompatActivity {
         messageAdapter = new MessageAdapter(this, messages, myID);
         chatRecyclerView.setAdapter(messageAdapter);
 
+    }
+
+    public void insert(String senderID, String receiverID, String message) {
+        String key = senderID+"@"+receiverID;
+        RequestBody formBody = new FormBody.Builder()
+                .add("id", key)
+                .add("message", message)
+                .build();
+        Request request = new Request.Builder()
+                .url("https://lamp.ms.wits.ac.za/home/s2542012/insertMessage.php")
+                .post(formBody)
+                .build();
+
+        OkHttpClient client = new OkHttpClient();
+        client.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    System.out.println(response.message());
+                    throw new IOException();
+                }
+                else {
+                    System.out.println();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+        });
     }
 }
