936d8b144dfca1523fb9cd69a41574c238e53089
Yusuf <moejoe1756@gmail.com>
2023-06-10 15:29:28 +0200

    Version 3

--- app/src/main/java/com/example/counsellingapp/Message.java
+++ app/src/main/java/com/example/counsellingapp/Message.java
@@ -2,13 +2,21 @@ package com.example.counsellingapp;
 
 public class Message {
 
-    String message;
-    String senderID;
+    private String message;
+    private String senderID;
 
     public Message() {}
 
     public Message(String message, String senderID) {
         this.message = message;
         this. senderID = senderID;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getSenderID() {
+        return senderID;
     }
 }
--- app/src/main/java/com/example/counsellingapp/MessageAdapter.java
+++ app/src/main/java/com/example/counsellingapp/MessageAdapter.java
@@ -1,4 +1,88 @@
 package com.example.counsellingapp;
 
-public class MessageAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    Context context;
+    ArrayList<Message> messages;
+    String currentUserID;
+
+    public MessageAdapter(Context context, ArrayList<Message> messages, String currentUserID) {
+        this.context = context;
+        this.messages = messages;
+        this.currentUserID = currentUserID;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == 1) {
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+            View view = layoutInflater.inflate(R.layout.received, parent, false);
+            return new MessageAdapter.ReceivedViewHolder(view);
+        }
+        else {
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+            View view = layoutInflater.inflate(R.layout.sent, parent, false);
+            return new MessageAdapter.SentViewHolder(view);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Message currentMessage = messages.get(position);
+        if (holder instanceof SentViewHolder) {
+            RecyclerView.ViewHolder viewHolder = (SentViewHolder) holder;
+            ((SentViewHolder) holder).sentMessage.setText(currentMessage.getMessage());
+        }
+        else {
+            RecyclerView.ViewHolder viewHolder = (ReceivedViewHolder) holder;
+            ((ReceivedViewHolder) holder).receivedMessage.setText(currentMessage.getMessage());
+        }
+    }
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    public static class SentViewHolder extends RecyclerView.ViewHolder {
+
+        TextView sentMessage;
+
+        public SentViewHolder(@NonNull View itemView) {
+            super(itemView);
+            sentMessage = itemView.findViewById(R.id.sentMessage);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Message currentMessage = messages.get(position);
+        if (currentUserID.equals(currentMessage.getSenderID())) {
+            return 2;
+        }
+        else {
+            return 1;
+        }
+    }
+
+    public static class ReceivedViewHolder extends RecyclerView.ViewHolder {
+
+        TextView receivedMessage;
+
+        public ReceivedViewHolder(@NonNull View itemView) {
+            super(itemView);
+            receivedMessage = itemView.findViewById(R.id.receivedMessage);
+        }
+    }
 }
